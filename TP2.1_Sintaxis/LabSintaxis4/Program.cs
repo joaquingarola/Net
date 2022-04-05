457ae18180749653c9a30da8ec85a13028d922a8
Manuel Cantarini <manu100cantarini@gmail.com>
2022-04-05 15:53:34 -0300

    Resolucion ejercicio 4.1 Prueba

--- TP2.1_Sintaxis/LabSintaxis4/Program.cs
+++ TP2.1_Sintaxis/LabSintaxis4/Program.cs
@@ -10,9 +10,9 @@ namespace LabSintaxis4
         }
         static void ej01()
         {
-            Console.WriteLine("Ingrese el primer valor");
+            Console.WriteLine("Ingrese el primer valor:");
             int valor1 = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Ingrese el segundo valor");
+            Console.WriteLine("Ingrese el segundo valor:");
             int valor2 = Int32.Parse(Console.ReadLine());
 
             Console.WriteLine("El resultado de la suma de " + valor1 + " y " + valor2 + " es " + (valor1 + valor2));
