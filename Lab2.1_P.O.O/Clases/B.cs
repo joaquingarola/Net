d483822360b02c1cd6879ac81a93ce2a33a8a589
Manuel Cantarini <manu100cantarini@gmail.com>
2022-04-13 21:40:18 -0300

    Resolucion Ejercicio 1 P.O.O

---
+++ Lab2.1_P.O.O/Clases/B.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clases
+{
+    public class B : A
+    {
+        public String M4()
+        {
+            String p;
+            return p =  "Metodo del hijo invocado";
+
+        }
+        public B() {
+            NombreInstancia = "Instancia de B";
+        }
+
+    }
+}
