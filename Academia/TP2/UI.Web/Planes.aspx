aede8918cee3941b658fcec2056c032902752823
Manuel Ferraretto <manuferraretto86@gmail.com>
2022-10-18 12:30:05 -0300

    Se agrega ABMC Planes

---
+++ Academia/TP2/UI.Web/Planes.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Planes.aspx.cs" Inherits="UI.Web.Planes" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="bodyContentPlaceHolder" runat="server">
+
+    <asp:Panel ID="gridPanel" runat="server">
+        <asp:GridView ID="gvListadoPlanes" runat="server" AutoGenerateColumns="false"
+            SelectedRowStyle-BackColor="Black"
+            SelectedRowStyle-ForeColor="White"
+            DataKeyNames="ID"
+            OnSelectedIndexChanged="gridView_SelectedIndexChanged">
+            <Columns>
+                <asp:BoundField HeaderText="ID Plan" DataField="ID" />
+                <asp:BoundField HeaderText="Descripcion Plan" DataField="Descripcion" />
+                <asp:CommandField SelectText="Seleccionar" ShowSelectButton="True" /> 
+            </Columns>
+        </asp:GridView>
+    </asp:Panel>
+
+     <asp:Panel ID="gridActionsPanel" runat="server">
+        <asp:LinkButton ID="editarLinkButton" runat="server" OnClick="editarLinkButton_Click">Editar</asp:LinkButton>
+        <asp:LinkButton ID="eliminarLinkButton" runat="server" OnClick="eliminarLinkButton_Click">Eliminar</asp:LinkButton>
+        <asp:LinkButton ID="nuevoLinkButton" runat="server" OnClick="nuevoLinkButton_Click">Nuevo</asp:LinkButton>
+    </asp:Panel>
+
+    <asp:Panel ID="formPanel" Visible="false" runat="server">
+
+        <asp:Label ID="descripcionLabel" runat="server" Text="Descripcion: "></asp:Label>
+        <asp:TextBox ID="descripcionTextBox" runat="server"></asp:TextBox><br />
+        <asp:Label ID="Label1" runat="server" Text="ID Especialidad: "></asp:Label>
+        <asp:TextBox ID="IdEspecialidad" runat="server"></asp:TextBox>
+        <asp:RequiredFieldValidator 
+            ID="RequiredFieldValidatorDescripcion" 
+            runat="server"
+            ErrorMessage="Por favor ingrese una descripción" 
+            ControlToValidate="descripcionTextBox"
+            Display="Dynamic" 
+            ForeColor="red"
+            ValidationGroup="a"
+            SetFocusOnError="True">
+            *
+        </asp:RequiredFieldValidator>
+        <br />
+
+        <asp:ValidationSummary 
+            runat="server" 
+            ID="ValidationSummary1" 
+            DisplayMode="BulletList"
+            ShowMessageBox="False" 
+            ForeColor="red"
+            ValidationGroup="a"
+            ShowSummary="True"
+        />
+
+        <asp:Panel ID="formActionsPanel" runat="server">
+            <asp:LinkButton ID="aceptarLinkButton" ValidationGroup="a" runat="server" OnClick="aceptarLinkButton_Click">Aceptar</asp:LinkButton>
+            <asp:LinkButton ID="cancelarLinkButton" runat="server" OnClick="cancelarLinkButton_Click">Cancelar</asp:LinkButton>
+        </asp:Panel>
+    </asp:Panel>
+
+
+
+
+
+
+</asp:Content>
---
+++ Academia/TP2/UI.Web/Planes.aspx.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Business.Entities;
+using Business.Logic;
+
+namespace UI.Web
+{
+    public partial class Planes : System.Web.UI.Page
+    {
+        PlanLogic _logic;
+
+        public enum FormModes
+        {
+            Alta,
+            Baja,
+            Modificacion
+        }
+
+        public FormModes FormMode
+        {
+            get { return (FormModes)this.ViewState["FormMode"]; }
+            set { this.ViewState["FormMode"] = value; }
+        }
+
+        private PlanLogic Logic
+        {
+            get
+            {
+                if (_logic == null)
+                {
+                    _logic = new PlanLogic();
+                }
+                return _logic;
+            }
+        }
+
+        private Plan Entity
+        {
+            get;
+            set;
+        }
+
+        private int SelectedID
+        {
+            get
+            {
+                if (this.ViewState["SelectedID"] != null)
+                {
+                    return (int)this.ViewState["SelectedID"];
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            set
+            {
+                this.ViewState["SelectedID"] = value;
+            }
+        }
+
+        private bool IsEntitySelected
+        {
+            get
+            {
+                return (this.SelectedID != 0);
+            }
+        }
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                LoadGrid();
+            }
+
+        }
+
+        protected void LoadGrid()
+        {
+            this.gvListadoPlanes.DataSource = this.Logic.GetAll();
+            this.gvListadoPlanes.DataBind();
+        }
+
+        private void loadForm(int id)
+        {
+            this.Entity = this.Logic.GetOne(id);
+            this.descripcionTextBox.Text = this.Entity.Descripcion;
+        }
+
+        private void EnableForm(bool enable)
+        {
+            this.descripcionTextBox.Enabled = enable;
+        }
+
+        private void ClearForm()
+        {
+            this.descripcionTextBox.Text = string.Empty;
+        }
+
+        private void LoadEntity(Plan plan)
+        {
+            plan.Descripcion = this.descripcionTextBox.Text;
+            plan.ID = int.Parse(this.IdEspecialidad.Text);
+        }
+
+        private void SaveEntity(Plan plan)
+        {
+            this.Logic.Save(plan);
+        }
+
+        private void DeleteEntity(int id)
+        {
+            this.Logic.Delete(id);
+        }
+
+        protected void gridView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.SelectedID = (int)this.gvListadoPlanes.SelectedValue;
+            this.formPanel.Visible = false;
+        }
+
+        protected void editarLinkButton_Click(object sender, EventArgs e)
+        {
+            if (this.IsEntitySelected)
+            {
+                this.formPanel.Visible = true;
+                this.EnableForm(true);
+                this.FormMode = FormModes.Modificacion;
+                this.loadForm(this.SelectedID);
+            }
+        }
+
+        protected void eliminarLinkButton_Click(object sender, EventArgs e)
+        {
+            if (this.IsEntitySelected)
+            {
+                this.formPanel.Visible = true;
+                this.FormMode = FormModes.Baja;
+                this.EnableForm(false);
+                this.loadForm(this.SelectedID);
+            }
+        }
+
+        protected void nuevoLinkButton_Click(object sender, EventArgs e)
+        {
+            this.formPanel.Visible = true;
+            this.FormMode = FormModes.Alta;
+            this.ClearForm();
+            this.EnableForm(true);
+        }
+
+        protected void aceptarLinkButton_Click(object sender, EventArgs e)
+        {
+            switch (this.FormMode)
+            {
+                case FormModes.Alta:
+                    this.Entity = new Plan();
+                    this.LoadEntity(this.Entity);
+                    this.SaveEntity(this.Entity);
+                    this.LoadGrid();
+                    break;
+                case FormModes.Baja:
+                    this.DeleteEntity(this.SelectedID);
+                    this.LoadGrid();
+                    this.gvListadoPlanes.SelectedIndex = -1;
+                    this.SelectedID = 0;
+                    break;
+                case FormModes.Modificacion:
+                    this.Entity = new Plan();
+                    this.Entity.ID = this.SelectedID;
+                    this.Entity.State = BusinessEntity.States.Modified;
+                    this.LoadEntity(this.Entity);
+                    this.SaveEntity(this.Entity);
+                    this.LoadGrid();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        protected void cancelarLinkButton_Click(object sender, EventArgs e)
+        {
+            this.ClearForm();
+            this.formPanel.Visible = false;
+        }
+    }
+}
